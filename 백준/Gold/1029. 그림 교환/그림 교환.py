e6acb5b4d513512d30a19ae8ebbd25a7bc34e25f
JaeHyeon Kim <89509933+JaeHyoNi@users.noreply.github.com>
2023-01-21 19:53:15 +0900

    [Gold I] Title: 그림 교환, Time: 468 ms, Memory: 191616 KB -BaekjoonHub

--- 백준/Gold/1029. 그림 교환/그림 교환.py
+++ 백준/Gold/1029. 그림 교환/그림 교환.py
@@ -1,19 +1,14 @@
 N = int(input())
 arr = [list(map(int, list(input()))) for _ in range(N)]
-dp = [[[False for _ in range(10)] for _ in range(1<<N)] for _ in range(N)]
+dp = [[[False for _ in range(10)] for _ in range(1 << N)] for _ in range(N)]
 answer = 0
-
-# 이전 방문한곳 / 방문기록 / 현재 그림 가격 / 사람 명수
 def dfs(pre, visited, val, num):
     global answer
     for i in range(1, N):
-        next_visit = visited | (1<<i)
-        if visited & (1<<i) > 0 or arr[pre][i] < val or dp[i][next_visit][arr[pre][i]]:
-            continue
-        else:
-            dp[i][next_visit][arr[pre][i]] = True
-            dfs(i, next_visit, arr[pre][i], num+1)
-            answer = max(answer, num+1)
-
+        next_visit, next_val = visited | (1<<i), arr[pre][i]
+        if visited & (1<<i) > 0 or next_val < val or dp[i][next_visit][next_val]: continue
+        dp[i][next_visit][next_val] = True
+        dfs(i, next_visit, next_val, num+1)
+        answer = max(answer, num+1)
 dfs(0, 1, 0, 1)
 print(answer)
