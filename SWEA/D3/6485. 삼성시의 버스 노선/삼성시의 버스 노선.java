b589281d48d8939ff8f6bc03f6af8c71890f3d3e
JaeHyeon Kim <89509933+JaeHyoNi@users.noreply.github.com>
2023-01-29 16:00:54 +0900

    [D3] Title: 삼성시의 버스 노선, Time: 200 ms, Memory: 36,712 KB -BaekjoonHub

---
+++ SWEA/D3/6485. 삼성시의 버스 노선/삼성시의 버스 노선.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int tc = scanner.nextInt();
+        for(int t=1; t<=tc; t++){
+            int N = scanner.nextInt();
+            int[] arr = new int[5001];
+            for(int i=0; i<N; i++){
+                int A = scanner.nextInt(), B = scanner.nextInt();
+                for(int k=A; k<=B; k++){
+                    arr[k] += 1;
+                }
+            }
+            int P = scanner.nextInt();
+            System.out.printf("#%d ",t);
+            for(int k=0; k<P; k++){
+                System.out.print(arr[scanner.nextInt()] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
