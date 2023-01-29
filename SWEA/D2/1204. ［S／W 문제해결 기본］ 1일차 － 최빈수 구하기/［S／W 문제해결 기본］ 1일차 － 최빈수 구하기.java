49c5391f8d106f593a391a9978498f14ba44f736
JaeHyeon Kim <89509933+JaeHyoNi@users.noreply.github.com>
2023-01-29 16:10:10 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 193 ms, Memory: 43,240 KB -BaekjoonHub

---
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int tc=1; tc<=T; tc++){
+            int Trash = sc.nextInt();
+            int[] arr = new int[101];
+            int answer=0, num=0;
+            int temp;
+            for(int i=0; i<1000; i++){
+                temp = sc.nextInt();
+                arr[temp] += 1;
+                if(arr[temp] > num){answer = temp; num = arr[temp];}
+                else{ if(arr[temp] == num && temp > answer){answer = temp; num=arr[temp];}}
+            }
+            System.out.printf("#%d %d\n", tc, answer);
+        }
+    }
+}
