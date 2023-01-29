65a72900fe1b856d1ecb5bf898512de8886ceb74
JaeHyeon Kim <89509933+JaeHyoNi@users.noreply.github.com>
2023-01-29 16:29:37 +0900

    [D3] Title: 부먹왕국의 차원 관문, Time: 787 ms, Memory: 125,208 KB -BaekjoonHub

---
+++ SWEA/D3/7964. 부먹왕국의 차원 관문/부먹왕국의 차원 관문.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int tc=1; tc<=T; tc++){
+            int N=sc.nextInt(), D=sc.nextInt();
+            int temp, answer = 0, now = D;
+            for(int i=1; i<=N; i++){
+                temp = sc.nextInt();
+                if(temp == 1){
+                    now = i+D;
+                }
+                if(i >= now){
+                    answer += 1;
+                    now += D;
+                }
+            }
+            System.out.printf("#%d %d\n",tc, answer);
+        }
+    }
+}
