b3a28e5c0a0739900ea907d84bc0b1add8257d84
JaeHyeon Kim <89509933+JaeHyoNi@users.noreply.github.com>
2023-02-01 12:26:47 +0900

    [D2] Title: 간단한 369게임, Time: 123 ms, Memory: 20,676 KB -BaekjoonHub

---
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int i=1; i<=T; i++) {
+			int K=i,temp;
+			String time = "";
+			while(K!=0) {
+				temp = K%10;
+				if(temp == 3 || temp == 6 || temp == 9) {time += "-"; }
+				K = K / 10;
+			}
+			if(!time.equals("")) {System.out.print(time+" ");}
+			else {System.out.print(i+" ");}
+		}		
+	}
+}
