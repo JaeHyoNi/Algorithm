2f13ae8725e648a1c7b9fff2c95009645bdcfd91
JaeHyeon Kim <89509933+JaeHyoNi@users.noreply.github.com>
2023-02-04 11:33:43 +0900

    [D3] Title: 이진수 표현, Time: 12 ms, Memory: 13,544 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.cpp
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main(void){
     int T; cin>>T;
     for(int i=0; i<T; i++){
-        int N, M; cin>>N>>M;
-        if( (((1<<N)-1) & M) == ((1<<N)-1)) cout<<"#"<<i+1<<" ON"<<endl;
-        else cout<<"#"<<i+1<<" OFF"<<endl;
+        int N, M; scanf("%d %d",&N,&M);
+        if( (((1<<N)-1) & M) == ((1<<N)-1)) printf("#%d ON\n",i+1);
+        else printf("#%d OFF\n",i+1);
     }
 }
