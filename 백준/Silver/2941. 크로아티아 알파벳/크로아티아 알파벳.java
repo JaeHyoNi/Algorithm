c5902f3473e24b3b12d316fd83127084d1cdb8db
JaeHyeon Kim <89509933+JaeHyoNi@users.noreply.github.com>
2023-02-07 23:41:04 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 108 ms, Memory: 12796 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] li = {"c=","c-","dz=","d-","lj","nj","s=","z="};
+        String str = sc.nextLine();
+        for(int i=0; i<8; i++){if(str.contains(li[i])) str = str.replaceAll(li[i],"0");}
+        System.out.println(str.length());
+    }
+}
