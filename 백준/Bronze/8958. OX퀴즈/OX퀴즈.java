a046fde81c4eb7de2fa7b2bcc154717cfc9b3985
JaeHyeon Kim <89509933+JaeHyoNi@users.noreply.github.com>
2023-02-08 00:18:26 +0900

    [Bronze II] Title: OX퀴즈, Time: 136 ms, Memory: 13456 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i=0; i<N; i++){
+            String str = sc.next();
+            int answer = 0, go=1;
+            for(int k=0; k<str.length(); k++){
+                if(str.charAt(k)=='O'){answer += go++;}
+                else{go = 1;}
+            }
+            System.out.println(answer);
+        }
+    }
+}
