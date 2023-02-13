ea3dac37ef122c34c9a71814e52f634cdbde939a
JaeHyeon Kim <89509933+JaeHyoNi@users.noreply.github.com>
2023-02-13 16:14:42 +0900

    [Gold V] Title: 탑, Time: 868 ms, Memory: 177808 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -3,39 +3,44 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
-
+class pair{
+	int x,y;
+	public pair(int x, int y) {
+		this.x = x; this.y = y;
+	}
+}
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder ans = new StringBuilder();
 		int[] answer = new int[T];
 		Integer[] arr = new Integer[T];
-		Stack<Integer[]> stack = new Stack<Integer[]>(); 
+		Stack<pair> stack = new Stack<pair>(); 
 		for(int i=0; i<T; i++) {arr[i]= Integer.parseInt(st.nextToken()); }
 		for(int i=0; i<T; i++) {
-			if(stack.isEmpty()) {stack.add(new Integer[] {i, arr[i]});}
-			else if(stack.peek()[1] > arr[i]) {
-				answer[i]= stack.peek()[0]; 
-				stack.add(new Integer[] {i, arr[i]});
+			if(stack.isEmpty()) {stack.add(new pair(i, arr[i]));}
+			else if(stack.peek().y > arr[i]) {
+				answer[i]= stack.peek().x; 
+				stack.add(new pair(i, arr[i]));
 			}
-			else if(stack.peek()[1] < arr[i]) {
-				Integer[] temp;
-				while(!stack.empty() && stack.peek()[1] <= arr[i]) {
+			else if(stack.peek().y < arr[i]) {
+				pair temp;
+				while(!stack.empty() && stack.peek().y <= arr[i]) {
 					temp = stack.pop();
 					if(stack.empty()) break;
-					answer[temp[0]] = stack.peek()[0] + 1;
+					answer[temp.x] = stack.peek().x + 1;
 				}
-				stack.add(new Integer[] {i, arr[i]});
+				stack.add(new pair(i, arr[i]));
 			}
 		}
 		while(!stack.empty()) {
-			int temp = stack.pop()[0];
+			int temp = stack.pop().x;
 			if(stack.empty()) break;
-			answer[temp] = stack.peek()[0] + 1;
+			answer[temp] = stack.peek().x + 1;
 		}
-		for(int i = 0; i< answer.length; i++) {
-			System.out.print(answer[i]+" ");
-		}
+		for(int i = 0; i< answer.length; i++) {ans.append(answer[i]+" ");}
+		System.out.println(ans.toString());
 	}
 }
