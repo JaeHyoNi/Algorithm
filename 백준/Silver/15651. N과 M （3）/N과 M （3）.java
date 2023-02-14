4f693dd888099bfb90d3bd884e7e13c838474a87
JaeHyeon Kim <89509933+JaeHyoNi@users.noreply.github.com>
2023-02-14 12:41:12 +0900

    [Silver III] Title: N과 M (3), Time: 640 ms, Memory: 315476 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+	static int[] arr;
+	static int N, M;
+	static StringBuilder ans = new StringBuilder();
+	public static void permu(int dep) {
+		if(dep == M) {
+			for(int i=0; i<M; i++) ans.append(arr[i]+" ");
+			ans.append("\n");
+			return;
+		}
+		for(int i=0; i<N; i++) {
+			arr[dep]= i+1;
+			permu(dep+1);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt(); M = sc.nextInt();
+		arr = new int[M];
+		permu(0);
+		System.out.println(ans.toString());
+	}
+}
