70ddfad720ef1d2c36afeb9e9d201656d978c9d4
JaeHyeon Kim <89509933+JaeHyoNi@users.noreply.github.com>
2023-02-21 17:30:35 +0900

    [Gold V] Title: 월드컵, Time: 116 ms, Memory: 12924 KB -BaekjoonHub

--- 백준/Gold/6987. 월드컵/월드컵.java
+++ 백준/Gold/6987. 월드컵/월드컵.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main{
 	static int[][] arr;
+	static int[] dt1 = {0, 1, 2}, dt2 = {2, 1, 0};
 	static boolean flag;
 	public static void dfs(int t1, int t2) {
 		if(t1 == 5 && t2 == 6) {
@@ -10,22 +11,15 @@ public class Main {
 			return;
 		}
 		if(t2 == 6) {dfs(t1+1, t1+2); return;}
-		if(arr[t1][0]>=1 && arr[t2][2] >=1 && !flag) {
-			arr[t1][0]-=1; arr[t2][2]-=1;
-			dfs(t1, t2+1);
-			arr[t1][0]+=1; arr[t2][2]+=1;
-		}
-		if(arr[t1][1]>=1 && arr[t2][1] >=1 && !flag) {
-			arr[t1][1]-=1; arr[t2][1]-=1;
-			dfs(t1, t2+1);
-			arr[t1][1]+=1; arr[t2][1]+=1;
-		}
-		if(arr[t1][2]>=1 && arr[t2][0] >=1 && !flag) {
-			arr[t1][2]-=1; arr[t2][0]-=1;
-			dfs(t1, t2+1);
-			arr[t1][2]+=1; arr[t2][0]+=1;
+		for(int i=0; i<3; i++) {
+			if(arr[t1][dt1[i]] >= 1 && arr[t2][dt2[i]] >= 1 && !flag) {
+				arr[t1][dt1[i]] -= 1;  arr[t2][dt2[i]]-= 1; 
+				dfs(t1, t2+1);
+				arr[t1][dt1[i]] += 1;  arr[t2][dt2[i]]+= 1; 
+			}
 		}
 	}
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		for(int k=0; k<4; k++) {
