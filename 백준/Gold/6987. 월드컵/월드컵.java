8337f7522f0725c5c9c56383991f44f996cf7e35
JaeHyeon Kim <89509933+JaeHyoNi@users.noreply.github.com>
2023-02-21 17:27:26 +0900

    [Gold V] Title: 월드컵, Time: 124 ms, Memory: 12912 KB -BaekjoonHub

---
+++ 백준/Gold/6987. 월드컵/월드컵.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+	static int[][] arr;
+	static boolean flag;
+	public static void dfs(int t1, int t2) {
+		if(t1 == 5 && t2 == 6) {
+			for(int i=0; i<6; i++) {for(int j=0; j<3; j++) {if(arr[i][j]!= 0) return;}}
+			flag = true;
+			return;
+		}
+		if(t2 == 6) {dfs(t1+1, t1+2); return;}
+		if(arr[t1][0]>=1 && arr[t2][2] >=1 && !flag) {
+			arr[t1][0]-=1; arr[t2][2]-=1;
+			dfs(t1, t2+1);
+			arr[t1][0]+=1; arr[t2][2]+=1;
+		}
+		if(arr[t1][1]>=1 && arr[t2][1] >=1 && !flag) {
+			arr[t1][1]-=1; arr[t2][1]-=1;
+			dfs(t1, t2+1);
+			arr[t1][1]+=1; arr[t2][1]+=1;
+		}
+		if(arr[t1][2]>=1 && arr[t2][0] >=1 && !flag) {
+			arr[t1][2]-=1; arr[t2][0]-=1;
+			dfs(t1, t2+1);
+			arr[t1][2]+=1; arr[t2][0]+=1;
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		for(int k=0; k<4; k++) {
+			flag = false;
+			arr = new int[6][3];
+			for(int i=0; i<6; i++) {
+				arr[i][0] = sc.nextInt(); arr[i][1] = sc.nextInt(); arr[i][2] = sc.nextInt();
+			}
+			dfs(0, 1);
+			if(flag) System.out.print("1 ");
+			else System.out.print("0 ");
+		}
+	}
+}
