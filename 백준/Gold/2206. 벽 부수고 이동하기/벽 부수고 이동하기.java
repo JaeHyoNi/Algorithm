220013aa1e06b8524806ac71495735d73a8d6e52
JaeHyeon Kim <89509933+JaeHyoNi@users.noreply.github.com>
2023-02-23 15:01:32 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 604 ms, Memory: 93616 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -2,15 +2,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
+	static class point{
+		int x, y, crush;
+		public point(int x, int y, int crush) {
+			this.x = x; this.y = y; this.crush = crush;
+		}
+	}
+	static int[][][] crush;
+	static char[][] arr;
+	static int[] dx = {-1, 0, 1, 0}, dy = {0, 1, 0, -1};
+	static int nx, ny, iter = 1;
+	static point n;
 	public static void main(String[] args) throws IOException {
-		int[][][] crush;
-		char[][] arr;
-		int[] dx = {-1, 0, 1, 0}, dy = {0, 1, 0, -1};
-		int nx, ny, iter = 1;
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(bf.readLine());
 		String tmp;
@@ -19,10 +25,9 @@ public class Main {
 		for(int i=0; i<N; i++) {
 			arr[i]= bf.readLine().toCharArray(); 
 		}
-		Integer[] n;
-		Queue<Integer[]> wait = new ArrayDeque<Integer[]>();
+		ArrayDeque<point> wait = new ArrayDeque<point>();
 		// {x, y, 벽뚫기 사용}
-		wait.add(new Integer[] {0, 0, 0});
+		wait.add(new point(0, 0, 0));
 		if(N==1 && M==1) {System.out.println("1"); System.exit(0);}
 		while(!wait.isEmpty()) {
 			siz = wait.size();
@@ -30,24 +35,24 @@ public class Main {
 			for(int k=0; k<siz; k++) {
 				n = wait.poll();
 				for(int i=0; i<4; i++) {
-					nx = n[0] + dx[i]; ny = n[1] + dy[i];
+					nx = n.x + dx[i]; ny = n.y + dy[i];
 					if(nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
-					if(arr[nx][ny]=='1' && n[2] == 0 && crush[nx][ny][1] == 0) {
+					if(arr[nx][ny]=='1' && n.crush == 0 && crush[nx][ny][1] == 0) {
 						if(nx == N-1 && ny == M-1) {
 							System.out.println(iter);
 							System.exit(0);
 						}
 						crush[nx][ny][1]= 1;
-						wait.add(new Integer[] {nx, ny, 1});
+						wait.add(new point(nx, ny, 1));
 						continue;
 					}
-					if(arr[nx][ny]=='0' && crush[nx][ny][n[2]] == 0) {
+					if(arr[nx][ny]=='0' && crush[nx][ny][n.crush] == 0) {
 						if(nx == N-1 && ny == M-1) {
 							System.out.println(iter);
 							System.exit(0);
 						}
-						crush[nx][ny][n[2]] = 1;
-						wait.add(new Integer[] {nx, ny, n[2]});
+						crush[nx][ny][n.crush] = 1;
+						wait.add(new point(nx, ny, n.crush));
 					}
 				}
 			}
