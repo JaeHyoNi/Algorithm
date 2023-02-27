bb172c4a27e7f241cbdea8e97e2496d9257d1db2
JaeHyeon Kim <89509933+JaeHyoNi@users.noreply.github.com>
2023-02-27 16:21:35 +0900

    [Platinum V] Title: 곡선 자르기, Time: 1432 ms, Memory: 249848 KB -BaekjoonHub

--- 백준/Platinum/14865. 곡선 자르기/곡선 자르기.py
+++ 백준/Platinum/14865. 곡선 자르기/곡선 자르기.py
@@ -1,17 +1,14 @@
 N = int(input())
-fx, fy = 9999999,9999999
+fx, fy = 9999999, 9999999
 arr = dict()
-ord = []
-str = []
+ord, str, point = [], [], []
 first = 0
 flag = False
-point = []
 for i in range(N):
     t1, t2 = map(int, input().split())
     point.append((t1, t2))
     if t1 < fx:
-        fx = t1
-        fy = t2
+        fx, fy = t1, t2
         first = i
     elif t1 == fx:
         if t2 < fy:
@@ -22,25 +19,19 @@ tmp = 0
 for i in range(first, first+N):
     x, y = point[i%N]
     if py * y >= 0:
-        px = x
-        py = y
+        px,py = x, y
         continue
     if not flag:
         flag = True
-        ord.append(x)
         tmp = x
     elif flag:
         if x > tmp:
-            arr[x] = ')'
-            arr[tmp] = '('
+            arr[x], arr[tmp] = ')', '('
         else:
-            arr[x] = '('
-            arr[tmp] = ')'
-        ord.append(x)
+            arr[x], arr[tmp] = '(', ')'
         flag = False
-    px = x
-    py = y
-
+    px, py = x, y
+    ord.append(x)
 for i in sorted(ord):
     str.append(arr[i])
 a, b, tmp = 0, 0, 0
@@ -48,10 +39,8 @@ k = len(str)
 for i in range(k):
     if str[i] == '(':
         tmp += 1
-        if str[i+1] == ')':
-            b += 1
+        if str[i+1] == ')': b += 1
     else:
-        if tmp == 1:
-            a += 1
+        if tmp == 1: a += 1
         tmp -= 1
 print(a, b)
