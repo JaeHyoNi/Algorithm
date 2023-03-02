596eac52dbf2907e876e6a820e038c511e377f77
JaeHyeon Kim <89509933+JaeHyoNi@users.noreply.github.com>
2023-03-02 14:22:27 +0900

    [Gold IV] Title: 최단경로, Time: 2080 ms, Memory: 129828 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,25 +1,22 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Main{
 	static class p implements Comparable<p>{int to; int val; public p(int to, int val) {this.to=to; this.val=val;}
 
 	@Override
 	public int compareTo(p o) {
 		return this.val - o.val;
 	}}
-	static int[] road;
-	static ArrayList<p>[] arr;
-	static StringBuilder sb = new StringBuilder();
-	static PriorityQueue<p> wait = new PriorityQueue<>();
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int V=Integer.parseInt(st.nextToken()), E=Integer.parseInt(st.nextToken());
 		int s = Integer.parseInt(br.readLine());
-		road = new int[V+1];
-		arr= new ArrayList[V+1];
-		for(int i=1; i<=V; i++) {arr[i]= new ArrayList<>(); road[i]= 999999;} 
+		int[] road = new int[V+1];
+		List<p> arr[]= new ArrayList[V+1];
+		for(int i=1; i<=V; i++) arr[i]= new ArrayList<>(); 
+		Arrays.fill(road, 999999);
 		road[s]= 0; 
 		for(int i=1; i<=E; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -28,6 +25,7 @@ public class Main {
 			int val=Integer.parseInt(st.nextToken());
 			arr[from].add(new p(to, val));
 		}
+		PriorityQueue<p> wait = new PriorityQueue<>();
 		wait.offer(new p(s, 0));
 		while(!wait.isEmpty()) {
 			p tmp = wait.poll();
@@ -39,10 +37,10 @@ public class Main {
 					wait.offer(new p(tmp2.to, road[tmp.to]+ tmp2.val ));
 			}
 		}
+		StringBuilder sb = new StringBuilder();
 		for(int i=1; i<=V; i++) {
-			if(road[i]==999999) sb.append("INF\n");
-			else sb.append(road[i]+"\n");
+			sb.append((road[i]== 999999 ? "INF" : road[i]) + "\n");
 		}
-		System.out.println(sb);
+		System.out.println(sb.toString());
 	}
 }
