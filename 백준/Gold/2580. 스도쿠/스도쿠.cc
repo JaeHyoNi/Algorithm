f106c736351b28d4dbeb2057b2d8b3eac1a26be5
JaeHyeon Kim <89509933+JaeHyoNi@users.noreply.github.com>
2023-04-02 22:02:50 +0900

    [Gold IV] Title: 스도쿠, Time: 64 ms, Memory: 2056 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.cc
+++ 백준/Gold/2580. 스도쿠/스도쿠.cc
@@ -1,49 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-int arr[10][10],siz;
-bool row[10][10],col[10][10],block[10][10];
-vector<pair<int,int>> zero;
-int square_num(int i,int j){
-    return (i/3)*3 + j/3;
-}
-void print(){
-    for(int i=0;i<9;i++){
-        for(int j=0;j<9;j++){
-            cout<<arr[i][j]<<" ";
-        }cout<<endl;
-    }
-}
-
-bool dfs(int n){
-    if(n == siz){print();return true;}
-    int x = zero[n].first;
-    int y = zero[n].second;
-    for(int i=1;i<=9;i++){
-        if(row[x][i] == false && col[y][i] == false && block[square_num(x,y)][i] == false){
-            row[x][i] = col[y][i] = block[square_num(x,y)][i] = true;
+int arr[9][9], r[9], c[9], m[9], siz;
+vector<pair<int,int>> p;
+void sol(int k){
+    if(k == siz){for(int i=0;i<9;i++){for(int j=0;j<9;j++){cout<<arr[i][j]<<" ";}cout<<"\n";}exit(0);}
+    int x = p[k].first, y=p[k].second;
+    for(int i=1; i<10; i++){
+        int t = (x/3)*3+y/3;
+        if((r[x] & (1 << i)) == 0 && (c[y] & (1 << i)) == 0 && (m[t] & (1 << i)) == 0) {
+            int a = r[x], b = c[y], n = m[(x / 3) * 3 + y / 3];
+            r[x] |= (1 << i); c[y] |= (1 << i); m[t] |= (1 << i);
             arr[x][y] = i;
-            if(dfs(n+1)){return true;}
-            row[x][i] = col[y][i] = block[square_num(x,y)][i] = false;
+            sol(k + 1);
             arr[x][y] = 0;
+            r[x] = a; c[y] = b; m[t] = n;
         }
     }
-    return false;
 }
-
 int main(){
-    siz = 0;
-    for(int i=0;i<9;i++){
-        for(int j=0;j<9;j++){
-            scanf("%d",&arr[i][j]);
-            if(arr[i][j] != 0){
-                row[i][arr[i][j]] = col[j][arr[i][j]] = block[square_num(i,j)][arr[i][j]] = true;
-            }
-            else{
-                zero.push_back({i,j});
-                siz++;
-            }
+    for(int i=0; i<9; i++) for(int j=0; j<9; j++) {
+        scanf("%d",&arr[i][j]);
+        if(arr[i][j] != 0){
+            r[i] |= (1 << arr[i][j]);
+            c[j] |= (1 << arr[i][j]);
+            m[3*(i/3) + (j/3)] |= (1 << arr[i][j]);
         }
+        else {p.push_back({i,j}); siz++;}
     }
-    dfs(0);
-    return 0;
+    sol(0);
 }
