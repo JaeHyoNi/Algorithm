a26de2a2207f8c4068aa15047eef748820392685
JaeHyeon Kim <89509933+JaeHyoNi@users.noreply.github.com>
2023-04-13 09:14:56 +0900

    [unrated] Title: 택배 배달과 수거하기, Time: 36.26 ms, Memory: 17.6 MB -BaekjoonHub

--- 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -1,28 +1,11 @@
 def solution(cap, n, deliveries, pickups):
-    answer = 0
-    i, di, pi = n-1, n-1, n-1
-    while i >= 0:
-        if deliveries[i] != 0 or pickups[i] != 0:
-            answer += (i * 2 + 2)
-            tmp = cap
-            while di >= 0 and tmp > 0:
-                if deliveries[di] > tmp:
-                    deliveries[di] -= tmp
-                    tmp = 0
-                else:
-                    tmp -= deliveries[di]
-                    deliveries[di] = 0
-                    di -= 1
-            tmp = cap
-            while pi >= 0 and tmp > 0:
-                if pickups[pi] > tmp:
-                    pickups[pi] -= tmp
-                    tmp = 0
-                else:
-                    tmp -= pickups[pi]
-                    pickups[pi] = 0
-                    pi -= 1
-            i = max(pi, di)
-        else:
-            i -= 1
-    return answer
+    answer, N = 0, 0
+    deliveries.append(0)
+    pickups.append(0)
+    for i in range(n-1, -1, -1):
+        deliveries[i] += deliveries[i+1]
+        pickups[i] += pickups[i+1]
+        while deliveries[i] > N or pickups[i] > N:
+            answer += (i+1)
+            N += cap
+    return answer*2
