f198d7af429304a5a40ed0064dd56d5f0682d196
JaeHyeon Kim <89509933+JaeHyoNi@users.noreply.github.com>
2023-04-25 20:42:25 +0900

    [Gold IV] Title: 알고스팟, Time: 168 ms, Memory: 116576 KB -BaekjoonHub

---
+++ 백준/Gold/1261. 알고스팟/알고스팟.py
@@ -0,0 +1,21 @@
+from collections import deque
+N, M = map(int, input().split())
+arr, ans = [list(list(map(int, list(input())))) for _ in range(M)], 0
+que0, que1 = deque([(0, 0)]), deque()
+dx, dy, arr[0][0] = [-1, 0, 1, 0], [0, 1, 0, -1], -1
+flip = False
+while True:
+    while que0:
+        x, y = que0.popleft()
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if nx < 0 or nx >= M or ny < 0 or ny >= N or arr[nx][ny] == -1: continue
+            if arr[nx][ny] == 0: que0.append((nx, ny))
+            else: que1.append((nx, ny))
+            arr[nx][ny] = -1
+    if arr[M-1][N-1] == -1: break
+    que0.extend(que1)
+    que1.clear()
+    ans += 1
+print(ans)
+
