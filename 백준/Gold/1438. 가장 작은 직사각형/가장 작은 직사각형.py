c59d9778fe1a076f5f14d9c3812e954109180c51
JaeHyeon Kim <89509933+JaeHyoNi@users.noreply.github.com>
2023-06-29 13:11:14 +0900

    [Gold II] Title: 가장 작은 직사각형, Time: 172 ms, Memory: 116192 KB -BaekjoonHub

--- 백준/Gold/1438. 가장 작은 직사각형/가장 작은 직사각형.py
+++ 백준/Gold/1438. 가장 작은 직사각형/가장 작은 직사각형.py
@@ -1,22 +1,10 @@
-from collections import deque
+from itertools import combinations_with_replacement
 N = int(input())
-arr = sorted([tuple(map(int, input().split())) for _ in range(N)])
-wait = deque([(min(t[0] for t in arr), min(t[1] for t in arr), max(t[0] for t in arr), max(t[1] for t in arr))])
-visit, A = set(), 1e10
-while wait:
-    lx, ly, rx, ry = wait.popleft()
-    if (lx, ly, rx, ry) in visit: continue
-    visit.add((lx, ly, rx, ry))
-    maxx, maxy, minx, miny, tmp = lx, ly, rx, ry, 0
-    for x, y in arr:
-        if lx <= x <= rx and ly <= y <= ry: tmp += 1
-        if lx < x < rx: minx, maxx = min(minx, x), max(maxx, x)
-        if ly < y < ry: miny, maxy = min(miny, y), max(maxy, y)
-    if tmp >= N//2:
-        if (rx - lx + 2) * (ry - ly + 2) < A:
-            A = (rx - lx + 2) * (ry - ly + 2)
-        wait.append((minx, ly, rx, ry))
-        wait.append((lx, miny, rx, ry))
-        wait.append((lx, ly, maxx, ry))
-        wait.append((lx, ly, rx, maxy))
+ps = [tuple(map(int, input().split())) for _ in range(N)]
+Y, N, A = sorted(list(set(p[1] for p in ps))), N//2, 1e10
+for y1, y2 in list(combinations_with_replacement(Y, 2)):
+    tmp = sorted([p[0] for p in ps if y1 <= p[1] <= y2])
+    if len(tmp) >= N:
+        for i in range(len(tmp) - N + 1):
+            A = min(A, (y2 - y1 + 2) * (tmp[i+N-1] - tmp[i] + 2))
 print(A)
